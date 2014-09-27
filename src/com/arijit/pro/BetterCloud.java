54e1e49d2565253b92606c78df5b1e3a8d80bcbe
ghoshbinayak <ghoshbinayak@gmail.com>
2014-09-27 19:04:26 +0530

    the cache is working

--- src/com/arijit/pro/BetterCloud.java
+++ src/com/arijit/pro/BetterCloud.java
@@ -1,21 +1,20 @@
 package com.arijit.pro;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-
 
 public class BetterCloud extends Activity  {
 
@@ -27,10 +26,10 @@ public class BetterCloud extends Activity  {
         setContentView(R.layout.loading);
         final TextView mTextView = (TextView) findViewById(R.id.webresponse);
         final ProgressBar spinner = (ProgressBar) findViewById(R.id.spinner);  
+        final ImageView mImageView = (ImageView) findViewById(R.id.canvas);
 
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
         String url ="http://www.google.com";
+        String imgurl = "https://farm6.staticflickr.com/5574/15138586768_9afee4767f.jpg";
 
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
@@ -50,8 +49,28 @@ public class BetterCloud extends Activity  {
                 mTextView.setVisibility(View.VISIBLE);
             }
         });
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+
+        // Retrieves an image specified by the URL, displays it in the UI.
+        ImageRequest imageRequest = new ImageRequest(imgurl,
+            new Response.Listener<Bitmap>() {
+                @Override
+                public void onResponse(Bitmap bitmap) {
+                    mImageView.setImageBitmap(bitmap);
+                    spinner.setVisibility(View.GONE);
+                    mImageView.setVisibility(View.VISIBLE);
+                }
+            }, 0, 0, null,
+            new Response.ErrorListener() {
+                public void onErrorResponse(VolleyError error) {
+                    mTextView.setText("That didn't work!");
+                    spinner.setVisibility(View.GONE);
+                    mTextView.setVisibility(View.VISIBLE);
+                }
+            });
+
+
+        // Get the Volley instance and add the request to the RequestQueue.
+        NetworkHandler.getInstance(this).addToRequestQueue(imageRequest);
 
         // Insert the back button 
         getActionBar().setDisplayHomeAsUpEnabled(true);
