c36208d62647287f18d4ad15263f7c1505a18092
ghoshbinayak <ghoshbinayak@gmail.com>
2014-10-04 18:33:02 +0530

    loading flickr images

--- src/com/arijit/pro/FlickrGallery.java
+++ src/com/arijit/pro/FlickrGallery.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.Display;
 import android.view.MenuItem;
 import android.view.Surface;
@@ -16,14 +17,24 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
 import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.VolleyError;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
 public class FlickrGallery extends Activity  {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         final Activity that = this;
@@ -32,9 +43,11 @@ public class FlickrGallery extends Activity  {
         setContentView(R.layout.gallery);
 
         // Set layout adapter
+        final ImageAdapter mImageAdapter = new ImageAdapter(this);
         GridView flickr_gallery = (GridView) findViewById(R.id.flickr_gallery);
-        flickr_gallery.setAdapter(new ImageAdapter(this));
+        flickr_gallery.setAdapter(mImageAdapter);
 
+        // Define onclick action
         flickr_gallery.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {                
@@ -44,6 +57,33 @@ public class FlickrGallery extends Activity  {
             }
         });
 
+        // Get image urls from flickr
+        String apiUrl = "https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=5c614dfbc3ba968e600ef21b0cebb014&format=json&nojsoncallback=1&auth_token=72157648340602662-5bb254dcc4dfbd61&api_sig=e4bbdfe73928cdc002ce4bb10222146d";
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+        (Request.Method.GET, apiUrl, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject photos = response.getJSONObject("photos");
+                    ArrayList<String> urlList = URLConstructor(photos.getJSONArray("photo"));
+                    mImageAdapter.addData(urlList);
+                }
+                catch (JSONException e){
+                    Log.e("JSONResponse", e.toString());
+                }
+                
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("JSONResponse", "something went wrong");
+
+            }
+        });
+        NetworkHandler.getInstance(this).addToRequestQueue(jsObjRequest);
+
         // Insert the back button 
         getActionBar().setDisplayHomeAsUpEnabled(true);
     }
@@ -52,11 +92,43 @@ public class FlickrGallery extends Activity  {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
         // Respond to the action bar's Up/Home button
-        case android.R.id.home:
+            case android.R.id.home:
             NavUtils.navigateUpFromSameTask(this);
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private ArrayList<String> URLConstructor(JSONArray photos){
+        int n = photos.length();
+        int i;
+        ArrayList<String> result = new ArrayList<String>();
+        String fullURL, farm, server, id, secret;
+        JSONObject photodata;
+        for (i=0; i < n; i++){
+            try {
+                photodata = photos.getJSONObject(i);
+                farm = photodata.getString("farm");
+                server = photodata.getString("server");
+                id = photodata.getString("id");
+                secret = photodata.getString("secret");
+                fullURL = "https://farm" 
+                        + farm
+                        + ".staticflickr.com/"
+                        + server
+                        + "/"
+                        + id
+                        + "_"
+                        + secret
+                        + "_q.jpg";
+                result.add(fullURL);
+            }
+            catch (JSONException e){
+                Log.e("JSONObject", "Error parsing: " + e.toString());
+            }
+        }
+
+        return result;
     }
 
     public class ImageAdapter extends BaseAdapter {
@@ -67,7 +139,7 @@ public class FlickrGallery extends Activity  {
 
         public ImageAdapter(Context c) {            
             mContext = c;
-       	    int displayWidth = c.getResources().getDisplayMetrics().widthPixels;
+            int displayWidth = c.getResources().getDisplayMetrics().widthPixels;
             Display display = ((WindowManager) c.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
             int rotation = display.getRotation();
             mImageLoader = NetworkHandler.getInstance(mContext).getImageLoader();
@@ -80,8 +152,8 @@ public class FlickrGallery extends Activity  {
         }
 
         public int getCount() {
-            //return mThumbIds.length;
-            return URLS.length;
+            // return URLS.length;
+            return URLS_FINAL.size();
         }
 
         public Object getItem(int position) {
@@ -100,36 +172,49 @@ public class FlickrGallery extends Activity  {
                 netImageView = new NetworkImageView(mContext);
                 netImageView.setLayoutParams(new GridView.LayoutParams(IMAGE_DIMENSION, IMAGE_DIMENSION));
                 netImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            } else {
+            } 
+            else {
                 netImageView = (NetworkImageView) convertView;
             }
 
-            netImageView.setImageUrl(URLS[position], mImageLoader);
+            // netImageView.setImageUrl(URLS[position], mImageLoader)  ;
+            netImageView.setImageUrl(URLS_FINAL.get(position), mImageLoader);
             netImageView.setDefaultImageResId(R.drawable.image_background);
+
+            // load more images when end is reached
+            // if(position == getCount() -1){
+            //     addData(new ArrayList<String>(Arrays.asList(URLS2)));
+            // }
             return netImageView;
+        }
+
+        // change url set
+        public void addData(ArrayList<String> newUrls){
+            URLS_FINAL.addAll(newUrls);
+            notifyDataSetChanged();
         }
 
         // references to our images
         private Integer[] mThumbIds = {
-                R.drawable.sample_2, R.drawable.sample_3,
-                R.drawable.sample_4, R.drawable.sample_5,
-                R.drawable.sample_6, R.drawable.sample_7,
-                R.drawable.sample_0, R.drawable.sample_1,
-                R.drawable.sample_2, R.drawable.sample_3,
-                R.drawable.sample_4, R.drawable.sample_5,
-                R.drawable.sample_6, R.drawable.sample_7,
-                R.drawable.sample_0, R.drawable.sample_1,
-                R.drawable.sample_2, R.drawable.sample_3,
-                R.drawable.sample_4, R.drawable.sample_5,
-                R.drawable.sample_6, R.drawable.sample_7,
-                R.drawable.sample_0, R.drawable.sample_1,
-                R.drawable.sample_2, R.drawable.sample_3,
-                R.drawable.sample_4, R.drawable.sample_5,
-                R.drawable.sample_6, R.drawable.sample_7,
-                R.drawable.sample_0, R.drawable.sample_1,
-                R.drawable.sample_2, R.drawable.sample_3,
-                R.drawable.sample_4, R.drawable.sample_5,
-                R.drawable.sample_6, R.drawable.sample_7
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
         };
 
         private String[] URLS = {
@@ -154,5 +239,43 @@ public class FlickrGallery extends Activity  {
             "http://lh6.ggpht.com/_QFsB_q7HFlo/TCItd_2oBkI/AAAAAAAAFsk/4lgJWweJ5N8/s144-c/3705226938_d6d66d6068_o.jpg",
             "http://lh5.ggpht.com/_JTI0xxNrKFA/TBsKQ9uOGNI/AAAAAAAChQg/z8Exh32VVTA/s144-c/CRW_0015-composite.jpg",
         };
+
+        private String[] URLS2 = {
+           "http://lh6.ggpht.com/_loGyjar4MMI/S-InVNkTR_I/AAAAAAAADJY/Fb5ifFNGD70/s144-c/Moving%20Rock.jpg",
+           "http://lh4.ggpht.com/_L7i4Tra_XRY/TBtxjScXLqI/AAAAAAAAE5o/ue15HuP8eWw/s144-c/opera%20house%20II.jpg",
+           "http://lh3.ggpht.com/_rfAz5DWHZYs/S9cstBTv1iI/AAAAAAAAeYA/EyZPUeLMQ98/s144-c/DSC_6425.jpg",
+           "http://lh6.ggpht.com/_iGI-XCxGLew/S-iYQWBEG-I/AAAAAAAACB8/JuFti4elptE/s144-c/norvig-polar-bear.jpg",
+           "http://lh3.ggpht.com/_M3slUPpIgmk/SlbnavqG1cI/AAAAAAAACvo/z6-CnXGma7E/s144-c/mf_003.jpg",
+           "http://lh4.ggpht.com/_loGyjar4MMI/S-InQvd_3hI/AAAAAAAADIw/dHvCFWfyHxQ/s144-c/Rainbokeh.jpg",
+           "http://lh4.ggpht.com/_yy6KdedPYp4/SB5rpK3Zv0I/AAAAAAAAOM8/mokl_yo2c9E/s144-c/Point%20Reyes%20road%20.jpg",
+           "http://lh5.ggpht.com/_6_dLVKawGJA/SMwq86HlAqI/AAAAAAAAG5U/q1gDNkmE5hI/s144-c/mobius-glow.jpg",
+           "http://lh3.ggpht.com/_QFsB_q7HFlo/TCItc19Jw3I/AAAAAAAAFs4/nPfiz5VGENk/s144-c/4551649039_852be0a952_o.jpg",
+           "http://lh6.ggpht.com/_TQY-Nm7P7Jc/TBpjA0ks2MI/AAAAAAAABcI/J6ViH98_poM/s144-c/IMG_6517.jpg",
+           "http://lh3.ggpht.com/_rfAz5DWHZYs/S9cLAeKuueI/AAAAAAAAeYU/E19G8DOlJRo/s144-c/DSC_4397_8_9_tonemapped2.jpg",
+           "http://lh4.ggpht.com/_TQY-Nm7P7Jc/TBpi6rKfFII/AAAAAAAABbg/79FOc0Dbq0c/s144-c/david_lee_sakura.jpg",
+           "http://lh3.ggpht.com/_TQY-Nm7P7Jc/TBpi8EJ4eDI/AAAAAAAABb0/AZ8Cw1GCaIs/s144-c/Hokkaido%20Swans.jpg",
+           "http://lh3.ggpht.com/_1aZMSFkxSJI/TCIjB6od89I/AAAAAAAACHM/CLWrkH0ziII/s144-c/079.jpg",
+           "http://lh5.ggpht.com/_loGyjar4MMI/S-InWuHkR9I/AAAAAAAADJE/wD-XdmF7yUQ/s144-c/Colorado%20River%20Sunset.jpg",
+           "http://lh3.ggpht.com/_0YSlK3HfZDQ/TCExCG1Zc3I/AAAAAAAAX1w/9oCH47V6uIQ/s144-c/3138923889_a7fa89cf94_o.jpg",
+           "http://lh6.ggpht.com/_K29ox9DWiaM/TAXe4Fi0xTI/AAAAAAAAVIY/zZA2Qqt2HG0/s144-c/IMG_7100.JPG",
+           "http://lh6.ggpht.com/_0YSlK3HfZDQ/TCEx16nJqpI/AAAAAAAAX1c/R5Vkzb8l7yo/s144-c/4235400281_34d87a1e0a_o.jpg",
+           "http://lh4.ggpht.com/_8zSk3OGcpP4/TBsOVXXnkTI/AAAAAAAAAEo/0AwEmuqvboo/s144-c/yosemite_forrest.jpg",
+           "http://lh4.ggpht.com/_6_dLVKawGJA/SLZToqXXVrI/AAAAAAAAG5k/7fPSz_ldN9w/s144-c/coastal-1.jpg",
+           "http://lh4.ggpht.com/_WW8gsdKXVXI/TBpVr9i6BxI/AAAAAAABhNg/KC8aAJ0wVyk/s144-c/IMG_6233_1_2-2.jpg",
+           "http://lh3.ggpht.com/_loGyjar4MMI/S-InS0tJJSI/AAAAAAAADHU/E8GQJ_qII58/s144-c/Windmills.jpg",
+           "http://lh4.ggpht.com/_loGyjar4MMI/S-InbXaME3I/AAAAAAAADHo/4gNYkbxemFM/s144-c/Frantic.jpg",
+           "http://lh5.ggpht.com/_loGyjar4MMI/S-InKAviXzI/AAAAAAAADHA/NkyP5Gge8eQ/s144-c/Rice%20Fields.jpg",
+           "http://lh3.ggpht.com/_loGyjar4MMI/S-InZA8YsZI/AAAAAAAADH8/csssVxalPcc/s144-c/Seahorse.jpg",
+           "http://lh3.ggpht.com/_syQa1hJRWGY/TBwkCHcq6aI/AAAAAAABBEg/R5KU1WWq59E/s144-c/Antelope.JPG",
+           "http://lh5.ggpht.com/_MoEPoevCLZc/S9fHzNgdKDI/AAAAAAAADwE/UAno6j5StAs/s144-c/c84_7083.jpg",
+           "http://lh4.ggpht.com/_DJGvVWd7IEc/TBpRsGjdAyI/AAAAAAAAFNw/rdvyRDgUD8A/s144-c/Free.jpg",
+           "http://lh6.ggpht.com/_iO97DXC99NY/TBwq3_kmp9I/AAAAAAABcz0/apq1ffo_MZo/s144-c/IMG_0682_cp.jpg",
+           "http://lh4.ggpht.com/_7V85eCJY_fg/TBpXudG4_PI/AAAAAAAAPEE/8cHJ7G84TkM/s144-c/20100530_120257_0273-Edit-2.jpg"
+        };
+
+        // private ArrayList<String> URLS_FINAL = new ArrayList<String>(Arrays.asList(URLS));
+        private ArrayList<String> URLS_FINAL = new ArrayList<String>();
     }
 };
+
+
